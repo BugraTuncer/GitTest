135130b57148f51d310e8828cac7f9e2b5027dbb
Buğra Tunçer <bugratuncer@bugra-macbook-air.local>
2018-10-24 21:18:10 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Buğra Tunçer on 24.10.2018.
+//  Copyright © 2018 Buğra Tunçer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
